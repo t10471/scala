19e47e9229ebe06a2f927463cc546f35b3766e4a
t10471 <t104711202@gmail.com>
2014-10-10 17:39:14 +0900

    added mysql project

---
+++ mysql/src/main/scala/Main.scala
@@ -0,0 +1,44 @@
+package org.mysql
+
+import java.sql.{DriverManager, Connection, Statement, ResultSet,SQLException}
+
+object Main {
+  def main(args: Array[String]) :Unit = {
+    println("Hello World by Scala")
+    try {
+      Class.forName("com.mysql.jdbc.Driver").newInstance();
+      var con = 
+        DriverManager.getConnection("jdbc:mysql://" + sys.env("MYSQL_PORT_3306_TCP_ADDR") +
+          "/hoge?" + 
+          "user=root" + 
+          "&password=" + sys.env("MYSQL_ENV_MYSQL_ROOT_PASSWORD")
+          );
+      try {
+        var stmt = con.createStatement()
+        var rs = stmt.executeQuery("SELECT * FROM test")
+        while (rs.next()){
+          print(rs.getString(1) + " ")
+          print(rs.getString(2) + " ")
+          print(rs.getString(3))
+        }
+        stmt.close()
+      } catch {
+        case e:SQLException => println("Database error "+e)
+
+        case e => {
+          println("Some other exception type:")
+          e.printStackTrace()
+        }
+        } finally {
+          con.close()
+        }
+      } catch {
+        case e:SQLException => println("Database error "+e)
+        case e => {
+          println("Some other exception type:")
+          e.printStackTrace()
+        }
+      }
+  }
+}
+
